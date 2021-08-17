8c70da94a1ebfdf1be10df0dbd57ad5cf0940de9
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-08-17 10:10:49 +0700

    .

--- src/ext/backend_comm.c
+++ src/ext/backend_comm.c
@@ -62,118 +62,125 @@ ResultCode sendEventsToApmServer( double serverTimeoutMilliseconds, const Config
         return resultFailure;
     }
     server.sun_family = AF_UNIX;
-    strcpy(server.sun_path, "/tmp/ElasticAPM/socket.sock");
+    strcpy(server.sun_path, "/tmp/ElasticAPM/apmdaemon.sock");
     if (connect(fd, (struct sockaddr *) &server, sizeof(struct sockaddr_un)) < 0) {
         php_printf("Error connect socket");
         close(fd);
         return resultFailure;
     }
 
-    long serverTimeoutMillisecondsLong = (long) ceil( serverTimeoutMilliseconds );
-    php_printf("%s\n", serializedEvents.begin);
-    ResultCode resultCode;
-    CURL* curl = NULL;
-    CURLcode result;
-    enum
-    {
-        authBufferSize = 256
-    };
-    char auth[authBufferSize];
-    enum
-    {
-        userAgentBufferSize = 100
-    };
-    char userAgent[userAgentBufferSize];
-    enum
-    {
-        urlBufferSize = 256
-    };
-    char url[urlBufferSize];
-    struct curl_slist* chunk = NULL;
-    int snprintfRetVal;
-    const char* authKind = NULL;
-    const char* authValue = NULL;
-
-    /* get a curl handle */
-    curl = curl_easy_init();
-    if ( curl == NULL )
-    {
-        ELASTIC_APM_LOG_ERROR( "curl_easy_init() returned NULL" );
-        resultCode = resultFailure;
-        goto failure;
+    if (write(fd, serializedEvents.begin, serializedEvents.length) < 0) {
+        php_printf("Error send data");
+        return resultFailure;
     }
 
+    return resultSuccess;
 
-    // Authorization with API key or secret token if present
-    if ( ! isNullOrEmtpyString( config->apiKey ) )
-    {
-        authKind = "ApiKey";
-        authValue = config->apiKey;
-    }
-    else if ( ! isNullOrEmtpyString( config->secretToken ) )
-    {
-        authKind = "Bearer";
-        authValue = config->secretToken;
-    }
+    // long serverTimeoutMillisecondsLong = (long) ceil( serverTimeoutMilliseconds );
+    // php_printf("%s\n", serializedEvents.begin);
+    // ResultCode resultCode;
+    // CURL* curl = NULL;
+    // CURLcode result;
+    // enum
+    // {
+    //     authBufferSize = 256
+    // };
+    // char auth[authBufferSize];
+    // enum
+    // {
+    //     userAgentBufferSize = 100
+    // };
+    // char userAgent[userAgentBufferSize];
+    // enum
+    // {
+    //     urlBufferSize = 256
+    // };
+    // char url[urlBufferSize];
+    // struct curl_slist* chunk = NULL;
+    // int snprintfRetVal;
+    // const char* authKind = NULL;
+    // const char* authValue = NULL;
 
-    if ( authValue != NULL )
-    {
-        snprintfRetVal = snprintf( auth, authBufferSize, "Authorization: %s %s", authKind, authValue );
-        if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
-        {
-            php_printf( "Failed to build Authorization header."
-                                   " snprintfRetVal: %d. authKind: %s. authValue: %s.\n", snprintfRetVal, authKind, authValue );
-            resultCode = resultFailure;
-            goto failure;
-        }
-        php_printf( "Adding header: %s\n", auth );
-        chunk = curl_slist_append( chunk, auth );
-    }
-    chunk = curl_slist_append( chunk, "Content-Type: application/x-ndjson" );
-    ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_HTTPHEADER, chunk );
+    // /* get a curl handle */
+    // curl = curl_easy_init();
+    // if ( curl == NULL )
+    // {
+    //     ELASTIC_APM_LOG_ERROR( "curl_easy_init() returned NULL" );
+    //     resultCode = resultFailure;
+    //     goto failure;
+    // }
 
-    // User agent - "elasticapm-<language>/<version>" (no separators between "elastic" and "apm" is on purpose)
-    // For example, "elasticapm-java/1.2.3" or "elasticapm-dotnet/1.2.3"
-    snprintfRetVal = snprintf( userAgent, userAgentBufferSize, "elasticapm-php/%s", PHP_ELASTIC_APM_VERSION );
-    if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
-    {
-        php_printf( "Failed to build User-Agent header. snprintfRetVal: %d\n", snprintfRetVal );
-        resultCode = resultFailure;
-        goto failure;
-    }
-    ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_USERAGENT, userAgent );
 
-    snprintfRetVal = snprintf( url, urlBufferSize, "%s/intake/v2/events", config->serverUrl );
-    if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
-    {
-        php_printf( "Failed to build full URL to APM Server's intake API. snprintfRetVal: %d\n", snprintfRetVal );
-        resultCode = resultFailure;
-        goto failure;
-    }
-    ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_URL, url );
+    // // Authorization with API key or secret token if present
+    // if ( ! isNullOrEmtpyString( config->apiKey ) )
+    // {
+    //     authKind = "ApiKey";
+    //     authValue = config->apiKey;
+    // }
+    // else if ( ! isNullOrEmtpyString( config->secretToken ) )
+    // {
+    //     authKind = "Bearer";
+    //     authValue = config->secretToken;
+    // }
 
-    result = curl_easy_perform( curl );
-    if ( result != CURLE_OK )
-    {
-        php_printf( "Sending events to APM Server failed. URL: `%s'. Error message: `%s'.\n", url, curl_easy_strerror( result ) );
-        resultCode = resultFailure;
-        goto failure;
-    }
+    // if ( authValue != NULL )
+    // {
+    //     snprintfRetVal = snprintf( auth, authBufferSize, "Authorization: %s %s", authKind, authValue );
+    //     if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
+    //     {
+    //         php_printf( "Failed to build Authorization header."
+    //                                " snprintfRetVal: %d. authKind: %s. authValue: %s.\n", snprintfRetVal, authKind, authValue );
+    //         resultCode = resultFailure;
+    //         goto failure;
+    //     }
+    //     php_printf( "Adding header: %s\n", auth );
+    //     chunk = curl_slist_append( chunk, auth );
+    // }
+    // chunk = curl_slist_append( chunk, "Content-Type: application/x-ndjson" );
+    // ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_HTTPHEADER, chunk );
 
-    long responseCode;
-    curl_easy_getinfo( curl, CURLINFO_RESPONSE_CODE, &responseCode );
-    php_printf( "Sent events to APM Server. Response HTTP code: %ld. URL: `%s'.\n", responseCode, url );
+    // // User agent - "elasticapm-<language>/<version>" (no separators between "elastic" and "apm" is on purpose)
+    // // For example, "elasticapm-java/1.2.3" or "elasticapm-dotnet/1.2.3"
+    // snprintfRetVal = snprintf( userAgent, userAgentBufferSize, "elasticapm-php/%s", PHP_ELASTIC_APM_VERSION );
+    // if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
+    // {
+    //     php_printf( "Failed to build User-Agent header. snprintfRetVal: %d\n", snprintfRetVal );
+    //     resultCode = resultFailure;
+    //     goto failure;
+    // }
+    // ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_USERAGENT, userAgent );
 
-    resultCode = resultSuccess;
+    // snprintfRetVal = snprintf( url, urlBufferSize, "%s/intake/v2/events", config->serverUrl );
+    // if ( snprintfRetVal < 0 || snprintfRetVal >= authBufferSize )
+    // {
+    //     php_printf( "Failed to build full URL to APM Server's intake API. snprintfRetVal: %d\n", snprintfRetVal );
+    //     resultCode = resultFailure;
+    //     goto failure;
+    // }
+    // ELASTIC_APM_CURL_EASY_SETOPT( curl, CURLOPT_URL, url );
 
-    finally:
-    if ( curl != NULL ) curl_easy_cleanup( curl );
+    // result = curl_easy_perform( curl );
+    // if ( result != CURLE_OK )
+    // {
+    //     php_printf( "Sending events to APM Server failed. URL: `%s'. Error message: `%s'.\n", url, curl_easy_strerror( result ) );
+    //     resultCode = resultFailure;
+    //     goto failure;
+    // }
 
-    ELASTIC_APM_LOG_DEBUG_FUNCTION_EXIT_MSG( "resultCode: %s (%d)", resultCodeToString( resultCode ), resultCode );
-    return resultCode;
+    // long responseCode;
+    // curl_easy_getinfo( curl, CURLINFO_RESPONSE_CODE, &responseCode );
+    // php_printf( "Sent events to APM Server. Response HTTP code: %ld. URL: `%s'.\n", responseCode, url );
 
-    failure:
-    goto finally;
+    // resultCode = resultSuccess;
+
+    // finally:
+    // if ( curl != NULL ) curl_easy_cleanup( curl );
+
+    // ELASTIC_APM_LOG_DEBUG_FUNCTION_EXIT_MSG( "resultCode: %s (%d)", resultCodeToString( resultCode ), resultCode );
+    // return resultCode;
+
+    // failure:
+    // goto finally;
 }
 
 #undef ELASTIC_APM_CURL_EASY_SETOPT
